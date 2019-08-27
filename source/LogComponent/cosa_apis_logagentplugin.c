ddf2f847f30a87494366b23b2f6c4dd97590d3f1
Simon Chung <simon.c.chung@accenture.com>
2019-08-27 10:54:48 +0100

    Revert "REFPLTB-371 : Added EthAgent and wifi module logger support in CcspLogAgent"
    This reverts commit 20e9a875c2e070718d5c6fcd664599cd4e0f72f7.
    
    Reason for revert: There is a new patchset
    
    Change-Id: Ifd1f917d5c82e5444cf00dae809783d0fc1455a8

--- source/LogComponent/cosa_apis_logagentplugin.c
+++ source/LogComponent/cosa_apis_logagentplugin.c
@@ -27,11 +27,7 @@
 #define CM_PROC_NAME "CcspCMAgentSsp"
 #define PSM_PROC_NAME "PsmSsp"
 #define PAM_PROC_NAME "CcspPandMSsp"
-#if !defined(_PLATFORM_RASPBERRYPI_)
 #define WIFI_PROC_NAME "wifilog_agent"
-#else
-#define WIFI_PROC_NAME "CcspWifiSsp"
-#endif
 #define Harvester_PROC_NAME "harvester"
 #define NOTIFY_PROC_NAME "notify_comp"
 #define PWRMGR_PROC_NAME "rdkbPowerManager"
@@ -209,6 +205,7 @@ if( AnscEqualString(ParamName, "X_RDKCENTRAL-COM_TR69_LogLevel", TRUE))
         *puLong  = ETHAGENT_RDKLogLevel;
         return TRUE;
     }
+
     return FALSE;
 }
 
@@ -398,9 +395,6 @@ LogAgent_SetParamUlongValue
 				AnscTraceWarning(("syscfg_commit failed\n"));
 			}
 		}
-#if defined(_PLATFORM_RASPBERRYPI_)
-		SendSignal(WIFI_PROC_NAME);
-#endif
 		return TRUE;
     }
 	if (AnscEqualString(ParamName, "X_RDKCENTRAL-COM_CR_LogLevel", TRUE))
@@ -927,11 +921,6 @@ LogAgent_SetParamBoolValue
         SendSignal(PWRMGR_PROC_NAME);
 		SW_Dealy();
         SendSignal(ETHAGENT_PROC_NAME);
-#if defined(_PLATFORM_RASPBERRYPI_)
-		SW_Dealy();
-        SendSignal(WIFI_PROC_NAME);
-		SW_Dealy();
-#endif
 		return TRUE;
     }
 	if (AnscEqualString(ParamName, "X_RDKCENTRAL-COM_TR69_LoggerEnable", TRUE))
@@ -1045,9 +1034,7 @@ LogAgent_SetParamBoolValue
 				AnscTraceWarning(("syscfg_commit failed\n"));
 			}
 		}
-#if defined(_PLATFORM_RASPBERRYPI_)
-		SendSignal(WIFI_PROC_NAME);
-#endif
+
 		return TRUE;
     }
 	if (AnscEqualString(ParamName, "X_RDKCENTRAL-COM_CR_LoggerEnable", TRUE))
