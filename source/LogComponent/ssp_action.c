e4f98824d310e26e9fafd7d0546fbff31f05a471
kkumar564 <kishore_kumar2@comcast.com>
2019-11-19 11:44:06 +0530

    TRDKB-326,RDKB-26266: Integrate support for XML to C in CcspLogAgent
    Reason for change: Size reduction
    Test Procedure: Ensure tr181 xml doesn't exist.
    Risks: None
    Signed-off-by: kishore_kumar2@comcast.com
    
    (cherry picked from commit bdc457815b10578f068f355a02f9f776788eb8dc)
    (cherry picked from commit ce7f394fa665c0d1b16ccc0eb70fa2dc806a7dae)
    Change-Id: I9937bcd0ae0623050cfecec3da499f20daf27c8c

--- source/LogComponent/ssp_action.c
+++ source/LogComponent/ssp_action.c
@@ -53,6 +53,7 @@
 #include "plugin_main.h"
 #include "dslh_dmagnt_interface.h"
 #include "ccsp_trace.h"
+#include "dm_pack_create_func.h"
 
 PDSLH_CPE_CONTROLLER_OBJECT     pDslhCpeController        = NULL;
 PCOMPONENT_COMMON_LOGAGENT          g_pComponent_COMMON_logagent  = NULL;
@@ -179,11 +180,11 @@ ssp_engage
     }
 
     returnStatus =
-        pDslhCpeController->RegisterCcspDataModel
+        pDslhCpeController->RegisterCcspDataModel2
             (
                 (ANSC_HANDLE)pDslhCpeController,
                 CrName, /* CCSP_DBUS_INTERFACE_CR,*/              /* CCSP CR ID */
-                CCSP_DATAMODEL_XML_FILE,             /* Data Model XML file. Can be empty if only base data model supported. */
+                DMPackCreateDataModelXML,             /* Data Model XML file. Can be empty if only base data model supported. */
                 CCSP_COMPONENT_NAME_LOGAGENT,            /* Component Name    */
                 CCSP_COMPONENT_VERSION_LOGAGENT,         /* Component Version */
                 CCSP_COMPONENT_PATH_LOGAGENT,            /* Component Path    */
